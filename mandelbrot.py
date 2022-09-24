5c5389364a50e806445a285bf54358e0b5b4f394
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2022-09-24 16:54:16 +0300

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,16 +1,15 @@
 # a simple Mandelbrot in python (slow)
 # for step by step explanation watch this video: https://youtu.be/mzizK6ms-gY
 
-import numpy as np
 import matplotlib.pylab as plt
-
-atoms=[]
 window_size = 300
 center = window_size/2
-
-mat = np.zeros((window_size, window_size))
+atoms=[]
+mat = []
 for y in range(window_size):
+    mat.append([])
     for x in range(window_size):
+        mat[y].append(0)
         dx = (x-center)/1000-0.12
         dy = (y-center)/1000-0.82
         a = dx
@@ -21,6 +20,30 @@ for y in range(window_size):
             a = d
             H = d > 200
             if(H==True):
-                mat[y, x] = t
-
+                mat[y][x] = t
 plt.imshow(mat)
+
+### another version with numpy (still not fast!)
+# import numpy as np
+# import matplotlib.pylab as plt
+
+# atoms=[]
+# window_size = 300
+# center = window_size/2
+
+# mat = np.zeros((window_size, window_size))
+# for y in range(window_size):
+#     for x in range(window_size):
+#         dx = (x-center)/1000-0.12
+#         dy = (y-center)/1000-0.82
+#         a = dx
+#         b = dy
+#         for t in range(50):
+#             d = (a*a)-(b*b)+dx
+#             b = 2*(a*b)+dy
+#             a = d
+#             H = d > 200
+#             if(H==True):
+#                 mat[y, x] = t
+
+# plt.imshow(mat)
